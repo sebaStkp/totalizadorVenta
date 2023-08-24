36c4e387494e8d115037dd08b517479b50d4852c
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2023-08-24 09:45:49 -0400

    se puede ingresar precio por item y mostrar en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -3,11 +3,15 @@
 const first = document.querySelector("#primer-numero");
 const form = document.querySelector("#tot-form");
 const div = document.querySelector("#resultado-div");
+const second = document.querySelector("#segundo-numero");
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + firstNumber +"</p>";
+  div.innerHTML = "<p>" + firstNumber +"</p>"  ;
+  div.innerHTML += "<p>" + secondNumber +"</p>";
 });
