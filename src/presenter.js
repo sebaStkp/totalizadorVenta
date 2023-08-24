dc000882bd601d834888f0b4b4f83cdfb49cd789
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2023-08-24 10:11:24 -0400

    se puede seleccionar estado y se muestra en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -4,6 +4,7 @@ const first = document.querySelector("#primer-numero");
 const form = document.querySelector("#tot-form");
 const div = document.querySelector("#resultado-div");
 const second = document.querySelector("#segundo-numero");
+const seleccion = document.querySelector("#est");
 
 
 form.addEventListener("submit", (event) => {
@@ -12,6 +13,7 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + firstNumber +"</p>"  ;
-  div.innerHTML += "<p>" + secondNumber +"</p>";
+  div.innerHTML = "<p>" + "Cantidad de items: " +firstNumber +"</p>"  ;
+  div.innerHTML += "<p>"+ "Precio por item: " + secondNumber +"</p>";
+  div.innerHTML += "<p>" + "Estado: "+ seleccion.value +"</p>";
 });
