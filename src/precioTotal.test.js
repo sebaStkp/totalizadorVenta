c94e0b5c8a200ad84259e28163947008873a8699
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2023-08-27 21:15:58 -0400

    Muestra el precio total pero solo considerando el impuesto que tiene el estado de TX

--- src/precioTotal.test.js
+++ src/precioTotal.test.js
@@ -4,5 +4,8 @@ describe("Calcula el precio total considerando solo impuesto por estado", () => 
     it("Muestra el precio total pero solo considerando el impuesto por el estado de CA ",() =>{
         expect(calcPrecioTot(800, "CA")).toEqual(866);
     });
+    it("Muestra el precio total pero solo considerando el impuesto por el estado de TX ",() =>{
+        expect(calcPrecioTot(800, "TX")).toEqual(850);
+    });
 });
 
