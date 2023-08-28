823b4b0ca2b7f9595a8a8147e2b17452af30e165
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2023-08-27 22:00:08 -0400

    Muestra el precio total(precio neto+impuesto-descuento)

--- src/precioTotal.js
+++ src/precioTotal.js
@@ -4,7 +4,7 @@ import descPorcentaje from "./descPorcentaje";
 function calcPrecioTot(pNeto, estado){
     const impuesto = retornarPorcentaje(estado)/100;
     const descuento = descPorcentaje(pNeto);
-    const result = pNeto + (pNeto * impuesto) + (pNeto * descuento);
+    const result = pNeto + (pNeto * impuesto) - (pNeto * descuento);
     return result;
 }
 
