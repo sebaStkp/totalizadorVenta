87d3bb3ca639c2ea544fe46778c8f701881d7472
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2023-08-27 21:17:27 -0400

    Muestra el precio total pero solo considerando el impuesto que tiene el estado de AL

--- src/precioTotal.test.js
+++ src/precioTotal.test.js
@@ -7,5 +7,8 @@ describe("Calcula el precio total considerando solo impuesto por estado", () => 
     it("Muestra el precio total pero solo considerando el impuesto por el estado de TX ",() =>{
         expect(calcPrecioTot(800, "TX")).toEqual(850);
     });
+    it("Muestra el precio total pero solo considerando el impuesto por el estado de AL ",() =>{
+        expect(calcPrecioTot(800, "AL")).toEqual(832);
+    });
 });
 
