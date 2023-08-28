b73a3946bab5832f67a1280068cf55a822ef1754
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2023-08-27 21:44:55 -0400

    Muestra el precio total considerando impuesto y descuento correspondiente a 3000

--- src/descPorcentaje.js
+++ src/descPorcentaje.js
@@ -1,15 +1,7 @@
 function descPorcentaje(pNeto){
     let result = 0;
-    if(pNeto >= 30000){
-        result= 0.15;
-    }
-    else if(pNeto >= 10000){
-        result= 0.1;
-    }
-    else if(pNeto >= 7000){
-        result= 0.07;
-    }
-    else if(pNeto >= 3000){
+    
+    if(pNeto >= 3000){
         result= 0.05;
     }
     else if(pNeto >= 1000){
--- src/precioTotal.test.js
+++ src/precioTotal.test.js
@@ -19,5 +19,8 @@ describe("Calcula el precio total considerando solo impuesto por estado", () => 
     it("Muestra el precio total considerando el impuesto y el descuento correspondiente a 1000 ",() =>{
         expect(calcPrecioTot(1000, "UT")).toEqual(1096.5);
     });
+    it("Muestra el precio total considerando el impuesto y el descuento correspondiente a 1000 ",() =>{
+        expect(calcPrecioTot(3000, "UT")).toEqual(3349.5);
+    });
 });
 
