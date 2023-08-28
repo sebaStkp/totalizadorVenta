86ca055427728cc1d7a90a27f83ae7ef2818b38d
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2023-08-28 19:03:27 -0400

    Errores arreglados en la parte del precioTotal.test.js

--- src/precioTotal.test.js
+++ src/precioTotal.test.js
@@ -17,19 +17,19 @@ describe("Calcula el precio total considerando solo impuesto por estado", () => 
         expect(calcPrecioTot(800, "UT")).toEqual(853.2);
     });
     it("Muestra el precio total considerando el impuesto y el descuento correspondiente a 1000 ",() =>{
-        expect(calcPrecioTot(1000, "UT")).toEqual(1096.5);
+        expect(calcPrecioTot(1000, "UT")).toEqual(1036.5);
     });
     it("Muestra el precio total considerando el impuesto y el descuento correspondiente a 3000 ",() =>{
-        expect(calcPrecioTot(3000, "UT")).toEqual(3349.5);
+        expect(calcPrecioTot(3000, "UT")).toEqual(3049.5);
     });
     it("Muestra el precio total considerando el impuesto y el descuento correspondiente a 7000 ",() =>{
-        expect(calcPrecioTot(7000, "UT")).toEqual(7955.5);
+        expect(calcPrecioTot(7000, "UT")).toEqual(6975.5);
     });
     it("Muestra el precio total considerando el impuesto y el descuento correspondiente a 10000 ",() =>{
-        expect(calcPrecioTot(10000, "UT")).toEqual(11665);
+        expect(calcPrecioTot(10000, "UT")).toEqual(9665);
     });
     it("Muestra el precio total considerando el impuesto y el descuento correspondiente a 30000 ",() =>{
-        expect(calcPrecioTot(30000, "UT")).toEqual(36495);
+        expect(calcPrecioTot(30000, "UT")).toEqual(27495);
     });
 });
 
